1aed1b9662d2c4abca9b3d3e0ba278890b5b9c23
k_krylowicz <kacper@krylowicz.pl>
2020-01-08 23:21:14 +0100

    changed heading font size

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 const Text = styled.h1`
   color: ${({ theme }) => theme.color.light};
   font-weight: ${({ theme }) => theme.medium};
+  font-size: 3rem;
 `;
 
 const Heading = ({ children }) => <Text>{children}</Text>;

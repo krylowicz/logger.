6e41989fd6268dba551aafa8b60f09933b906bca
k_krylowicz <kacper@krylowicz.pl>
2020-01-10 12:21:41 +0100

    configured firebase

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Firebase, { FirebaseContext } from 'actions';
 import HomeView from 'views/HomeView';
 import CreateAccountView from 'views/CreateAccountView';
 import LoginView from 'views/LoginView';
 
 const Root = () => (
-  <BrowserRouter>
-    <Route exact path="/" component={HomeView} />
-    <Route exact path="/login" component={LoginView} />
-    <Route exact path="/create-account" component={CreateAccountView} />
-  </BrowserRouter>
+  <FirebaseContext.Provider value={new Firebase()}>
+    <BrowserRouter>
+      <Route exact path="/" component={HomeView} />
+      <Route exact path="/login" component={LoginView} />
+      <Route exact path="/create-account" component={CreateAccountView} />
+    </BrowserRouter>
+  </FirebaseContext.Provider>
 );
 
 export default Root;
